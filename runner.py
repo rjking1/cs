cd959e77045730ebabcaeed5885f8d9496f6356c
Richard King <rjking58@gmail.com>
2021-11-19 12:05:17 +1100

    tidy, reformat and reduce sell amt to 99% of buy  toallow for fee?

--- runner.py
+++ runner.py
@@ -3,12 +3,14 @@ from config import *
 from time import sleep
 import random
 
+
 def buying_coin(coin, buy_orders):
     for buys in buy_orders:
         already_buying_coin = buys["coin"]
         if coin.upper() == already_buying_coin.upper():
             return True
     return False
+
 
 def cancel_coin(coin, buy_orders):
     for order in buy_orders:
@@ -23,7 +25,8 @@ latest_prices = cs.latest()["prices"]
 for latest_price in latest_prices:
     if latest_price.upper() in BUY_CODES:
         coin = latest_prices[latest_price]
-        print(latest_price, " bid ", coin["bid"], "ask", coin["ask"], "last", coin["last"])
+        print(latest_price, " bid ", coin["bid"],
+              "ask", coin["ask"], "last", coin["last"])
 
 balances = cs.my_balances()["balance"]
 print("--- Coin balances and approx value using last ---")
@@ -63,8 +66,10 @@ random.shuffle(BUY_CODES)
 for ucoin in BUY_CODES:
     coin = ucoin.lower()
 
-    rate = round((float(latest_prices[coin]["ask"]) * 0.1 + float(latest_prices[coin]["bid"]) * 0.9), 6)
-    amt = round(min(aud_avail, MAX_TRADE_AUD) / rate * 0.999, 6)  # 0.999 to reduce slightly so as not to exceed avail bal when rounding
+    rate = round(float(latest_prices[coin]["bid"]) * 0.9
+                 + float(latest_prices[coin]["ask"]) * 0.1, 6)
+    # 0.999 to reduce slightly so as not to exceed avail bal when rounding
+    amt = round(min(aud_avail, MAX_TRADE_AUD) / rate * 0.999, 6)
 
     if PRODUCTION:
         cs.my_buy(ucoin, amt, rate)
@@ -75,13 +80,13 @@ for ucoin in BUY_CODES:
     if aud_avail < MIN_TRADE_AUD:
         break
 
-#print(buys_added)        
+# print(buys_added)
 if PRODUCTION:
     sleep(30)
 
 # check if the buy orders were completed -- if buy order exists then nobody bought it so cancel it
 buy_orders = cs.my_orders()["buyorders"]
-#print(buy_orders)
+# print(buy_orders)
 for ucoin in buys_added:
     coin = ucoin.lower()
     if buying_coin(coin, buy_orders):
@@ -91,13 +96,12 @@ for ucoin in buys_added:
     else:
         # else completed=bought so create sell order
         # get buy order details for coin
-        amt=buys_added[ucoin]["amt"]
-        rate=buys_added[ucoin]["rate"]
-        # reduce sell amount slightly as rounding can affect things
-        amt = round(amt * 0.999, 6)
-        # and increase sell rate to compensate
-        sell_rate = rate * SELL_ABOVE_BUY
-        sell_rate = round(sell_rate * 1.001001, 6)
+        amt = buys_added[ucoin]["amt"]
+        rate = buys_added[ucoin]["rate"]
+        # reduce sell amount slightly to allow for fee???
+        amt = round(amt * 0.99, 6)
+        sell_rate = round(rate * SELL_ABOVE_BUY, 6)
         if PRODUCTION:
             cs.my_sell(ucoin, amt, sell_rate)
-        print("### created sell order for", ucoin, "amount:", amt, "rate", sell_rate)
+        print("### created sell order for", ucoin,
+              "amount:", amt, "rate", sell_rate)
